20bda906c1f2cc9ed0b5ce6e6fa306b98ae62515
Henry Dotson <l3iodeez@gmail.com>
2015-10-14 11:52:26 -0400

    Add Assosiations for User, Course.

--- AssociationsExercise/skeleton/app/models/user.rb
+++ AssociationsExercise/skeleton/app/models/user.rb
@@ -1,2 +1,29 @@
 class User < ActiveRecord::Base
+  has_many(
+      :enrollments,
+      class_name: "Enrollment",
+      foreign_key: :student_id,
+      primary_key: :id
+  )
+  has_many(
+    :enrolled_courses,
+    through: :enrollments,
+    source:  :course
+    )
+  has_many(
+    :instructors,
+    through: :enrolled_courses,
+    source:  :enrollments
+  )
+  has_many(
+    :taught_courses,
+    class_name: 'Course',
+    foreign_key: :instructor_id,
+    primary_key: :id
+  )
+  has_many(
+    :students,
+    through: :taught_courses,
+    source:  :enrollments
+  )
 end
